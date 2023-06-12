d2ae51529e3d7dc8b37871282f23a74951e1df8b
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-12 11:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ const getWeatherBtn = document.getElementById('getWeatherBtn');
+ const weatherDataDiv = document.getElementById('weatherData');
+
+        getWeatherBtn.addEventListener('click', async () => {
+            try {
+                const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=87e4d8152db8377e68f0144c06dbc0e3';
+
+                const response = await fetch(url);
+                const weatherData = await response.json();
+
+                const weatherDescription = weatherData.weather[0].description;
+
+                weatherDataDiv.textContent = `Current weather in London: ${weatherDescription}`;
+            } catch (error) {
+                console.log(error);
+            }
+        });
